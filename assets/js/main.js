f0ba402ca707a04d318bcc9d7e8fb33d6a63f4c6
Mohammad Ibrahim <md.kardi420@gmail.com>
2017-07-16 11:37:32 +0600

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,7 @@
     jQuery(document).ready(function($){
 
 
-        $(".embed-responsive iframe").addClass("embed-responsive-item");
-        $(".carousel-inner .item:first-child").addClass("active");
-        
-        $('[data-toggle="tooltip"]').tooltip();
+       
 
 
 
